c4b12bd9ba4f4689234717440cbb3caf94f82d76
Aziel Randel Rabano <imazielr@gmail.com>
2026-08-26 09:57:37 +0800

    cleaning update for hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -87,107 +87,109 @@ export default function Hero() {
 
   return (
     <section ref={trackRef} className="relative h-[150vh] pb-10">
-      {/* sticky top-0 h-screen` pins this content for parallax feel */}
-      <div className="sticky top-0 h-screen w-full px-4 overflow-hidden">
-        <div className="w-full max-w-[1280px] h-full mx-auto flex flex-col justify-between">
-          {/* heading layer */}
-          <motion.div
+      <div className="sticky top-0 w-full max-w-[1280px] 3xl:max-w-[1350px] h-screen mx-auto flex flex-col justify-between">
+        {/* heading layer */}
+        <motion.div className="relative w-full h-[100vh] flex flex-col items-center pt-20 pb-5">
+          <motion.h1
             style={{ opacity: headingOpacity }}
-            className="relative w-full flex flex-col justify-center items-center py-24"
+            className="text-[46px] lg:text-[120px] 3xl:text-[12px] font-extrabold text-center 
+                lg:leading-[1.10]"
           >
-            <h1 className="text-[46px] lg:text-[120px] 3xl:text-[174px] font-extrabold text-center lg:leading-[1.10] xl">
-              FULL STACK
-              <br />
-              <span className="text-transparent [-webkit-text-stroke:1.5px_var(--foreground)] lg:[-webkit-text-stroke:2.5px_var(--foreground)] 3xl:[-webkit-text-stroke:3.5px_var(--foreground)]">
-                DEVELOPER
-              </span>
-            </h1>
+            FULL STACK
+            <br />
+            <span
+              className="text-transparent 
+                [-webkit-text-stroke:1.5px_var(--foreground)] 
+                lg:[-webkit-text-stroke:2.5px_var(--foreground)] 
+                3xl:[-webkit-text-stroke:3.5px_var(--foreground)]"
+            >
+              DEVELOPER
+            </span>
+          </motion.h1>
 
-            {/* offset elements */}
+          {/* hero image card */}
+          <motion.div
+            style={{
+              scale: isDesktop ? scale : 1,
+              rotateY: isDesktop ? rotateY : 0,
+              opacity: isDesktop ? 1 : headingOpacity,
+              transformPerspective: 800,
+              transformStyle: "preserve-3d", // preserves the inner counter
+            }}
+            className="absolute bottom-40 m-auto w-44 h-52 rounded-lg overflow-hidden bg-gray-700 z-10"
+          >
+            {/* counter-rotated wrapper*/}
+            <motion.div
+              style={{ rotateY: isDesktop ? counterRotateY : 0 }}
+              className="relative w-full h-full"
+            >
+              <Image
+                src="/images/hero.png"
+                alt="hero"
+                fill
+                sizes="1"
+                className="object-cover"
+              />
+            </motion.div>
           </motion.div>
+        </motion.div>
 
-          {/* image + intro*/}
-          <div className="relative flex-1">
-            {/* intro layer - fills the whole stage, image renders over it */}
-            <div className="absolute inset-x-0 bottom-10 h-132 flex flex-col lg:flex-row justify-between items-center gap-8">
-              {/* left text */}
-              <div className="h-full flex flex-col justify-between max-w-sm">
-                <motion.p
-                  style={{ opacity: greetingOpacity, y: greetingY }}
-                  className="text-6xl lg:text-7xl font-medium"
-                >
-                  Oh, <br /> hi there.
-                </motion.p>
-                <motion.p
-                  style={{ opacity: restOpacity, y: restY }}
-                  className="text-xl font-light"
-                >
-                  I'm Aziel, A-S-I-Y-E-L. A full-stack developer blending
-                  creative thinking with logical structure.
-                </motion.p>
-              </div>
+        {/* intro*/}
+        <div
+          className="absolute inset-x-0 bottom-0 h-132 flex flex-col lg:flex-row 
+            justify-between items-center px-2 md:px-0 gap-8"
+        >
+          {/* left text */}
+          <div className="w-full h-full flex flex-col justify-between">
+            <motion.p
+              style={{ opacity: greetingOpacity, y: greetingY }}
+              className="w-full lg:max-w-sm text-6xl lg:text-7xl font-medium"
+            >
+              Oh, <br className="hidden lg:block" /> hi there.
+            </motion.p>
+            <motion.p
+              style={{ opacity: restOpacity, y: restY }}
+              className="text-xl font-light max-w-2xs"
+            >
+              I'm Aziel, A-S-I-Y-E-L. A full-stack developer blending creative
+              thinking with logical structure.
+            </motion.p>
+          </div>
 
-              {/* right text */}
-              <motion.div
-                style={{ opacity: restOpacity, y: restY }}
-                className="h-full flex flex-col justify-end max-w-sm text-xl font-light gap-4"
-              >
-                <p>
-                  I'm drawn to the space where design meets logic—building
-                  interfaces that feel intentional and systems that are built to
-                  solve real problems. From web applications to experimental
-                  projects, I'm always exploring new technologies and better
-                  ways to turn an idea into something people can actually use.
-                </p>
+          {/* right text */}
+          <motion.div
+            style={{ opacity: restOpacity, y: restY }}
+            className="w-full lg:max-w-2xs xl:max-w-sm h-full flex flex-col lg:justify-end 
+              text-xl font-light gap-4"
+          >
+            <p>
+              I'm drawn to the space where design meets logic—building
+              interfaces that feel intentional and systems that are built to
+              solve real problems. From web applications to experimental
+              projects, I'm always exploring new technologies and better ways to
+              turn an idea into something people can actually use.
+            </p>
 
-                <a href="#" className="group flex items-center gap-4">
-                  Get Started
-                  <span className="group relative w-7 h-7 flex justify-center items-center rounded-xl border border-black overflow-hidden">
-                    {/* Diagonal fill layer, scoped to this span only */}
-                    <span
-                      className="absolute inset-0 rounded-xl bg-black origin-bottom-left scale-0
+            <a href="#" className="group flex items-center gap-4">
+              Get Started
+              <span className="group relative w-7 h-7 flex justify-center items-center rounded-xl border border-black overflow-hidden">
+                {/* fill layer */}
+                <span
+                  className="absolute inset-0 rounded-xl bg-black origin-bottom-left scale-0
                         transition-transform duration-300 ease-out
                         group-hover:scale-100"
-                    />
-
-                    <Image
-                      src={"/icons/arrow2.png"}
-                      alt="arrow"
-                      width={50}
-                      height={50}
-                      className="relative z-10 w-5 h-auto object-contain invert group-hover:invert-0 transition duration-500"
-                    />
-                  </span>
-                </a>
-              </motion.div>
-            </div>
-
-            {/* hero image card */}
-            <motion.div
-              style={{
-                scale: isDesktop ? scale : 1,
-                rotateY: isDesktop ? rotateY : 0,
-                opacity: isDesktop ? 1 : headingOpacity,
-                transformPerspective: 800,
-                transformStyle: "preserve-3d", // preserves the inner counter
-              }}
-              className="absolute inset-0 m-auto w-44 h-52 mb-46 rounded-lg overflow-hidden bg-gray-700 z-10"
-            >
-              {/* counter-rotated wrapper*/}
-              <motion.div
-                style={{ rotateY: isDesktop ? counterRotateY : 0 }}
-                className="relative w-full h-full"
-              >
-                <Image
-                  src="/images/hero.png"
-                  alt="hero"
-                  fill
-                  sizes="1"
-                  className="object-cover"
                 />
-              </motion.div>
-            </motion.div>
-          </div>
+
+                <Image
+                  src={"/icons/arrow2.png"}
+                  alt="arrow"
+                  width={50}
+                  height={50}
+                  className="relative z-10 w-5 h-auto object-contain invert group-hover:invert-0 transition duration-500"
+                />
+              </span>
+            </a>
+          </motion.div>
         </div>
       </div>
     </section>
